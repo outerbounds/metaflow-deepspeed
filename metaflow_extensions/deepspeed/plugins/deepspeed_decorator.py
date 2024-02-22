ba50bab8973a00af321a89e89992e2b402fc4c53
Eddie Mattia <eddie@outerbounds.com>
2024-02-22 08:14:38 -0800

    propagate env vars, refactor s3 checkpoint

--- metaflow_extensions/deepspeed/plugins/deepspeed_decorator.py
+++ metaflow_extensions/deepspeed/plugins/deepspeed_decorator.py
@@ -5,7 +5,10 @@ from metaflow.plugins.parallel_decorator import (
 )
 from metaflow.exception import MetaflowException
 import metaflow
+
+# from metaflow import Run as MetaflowRun
 from functools import partial
+from typing import List, Dict, Union
 import subprocess
 import socket
 import json
@@ -20,10 +23,28 @@ DEEPSPEED_JOB_COMPLETE_VAR = "mpi_job_status"
 PUBLIC_KEY_RECEIVED_VAR = "public_key_received"
 CONTROL_TASK_DONE_PATH = "control"
 MPI_PUBLIC_KEY_PATH = "public_keys"
-DEEPSPEED_ENV_FILE = ".deepspeed_env" # https://github.com/microsoft/DeepSpeed/blob/24f20ef0a105d32f6085fe0d3b1c2f9324a6262c/docs/_tutorials/getting-started.md?plain=1#L230-L254
+DEEPSPEED_ENV_FILE = ".deepspeed_env"  # https://github.com/microsoft/DeepSpeed/blob/24f20ef0a105d32f6085fe0d3b1c2f9324a6262c/docs/_tutorials/getting-started.md?plain=1#L230-L254
+
 
 class DeepspeedExecutor:
-    def __init__(self, hosts, n_slots_per_host=1, is_gpu=False, flow=None, worker_polling_freq=10) -> None:
+    """
+    Instances of the DeepspeedExecutor class are responsible for launching the Deepspeed command. There is one per Metaflow @step annotated with @deepspeed.
+    DeepspeedExecutor consumes a list of hosts aka nodes aka k8s pods aka metaflow task containers, and information about how many processes to run on each.
+    In the constructor, the _scan_all_hosts function constructs the ~/.ssh/known_hosts list on each node, so they know how to communicate via passwordless ssh.
+
+    The Deepspeed decorator, which users specify in a Metaflow num_parallel task with @deepspeed, attaches an instance of this class to current.deepspeed.
+        Using current.deepspeed.run, users can run the same Deepspeed launch command they would independently of Metaflow.
+        This class will handle opening the subprocess, and ensuring other typical Metaflow functionality works as expected.
+    """
+
+    def __init__(
+        self,
+        hosts: List[str],
+        n_slots_per_host: int = 1,
+        is_gpu: bool = False,
+        flow=None,
+        worker_polling_freq: int = 10,
+    ) -> None:
         self.is_gpu = is_gpu
         self.n_slots_per_host = n_slots_per_host
         self.hosts = [
@@ -35,20 +56,22 @@ class DeepspeedExecutor:
 
     def _exec_cmd(
         self,
-        deepspeed_args=[],
-        entrypoint=None,
-        entrypoint_args=[],
-        deepspeed_config=None,
+        deepspeed_args: Union[List[str], Dict[str, str]] = [],
+        entrypoint: str = None,
+        entrypoint_args: Union[List[str], Dict[str, str]] = [],
+        # deepspeed_config = None,
     ):
         """
-        `deepspeed_args` : List[str] - arguments to pass to `exe`
-        `entrypoint`: str - program to run, such as compiled binary or python script
-        `entrypoint_args`: List[str] - arguments to pass after `entrypoint`
-        `deepspeed_config`: ... TODO json thing
+        deepspeed_args: Dict[str] - arguments to pass to `exe`
+        entrypoint: str - Python script for the Deepspeed launcher to run, such as a PyTorch or Huggingface training routine.
+        entrypoint_args: Dict[str] - arguments to pass after `entrypoint`
+        deepspeed_config: ... TODO
         """
 
+        # Container to build up the command to be run in a subprocess.
         cmd = ["deepspeed"]
 
+        # Construct the deepspeed distributed args.
         if "--hostfile" not in deepspeed_args:
             deepspeed_args.extend(["--hostfile", HOSTFILE])
         if "--num_nodes" not in deepspeed_args:
@@ -59,106 +82,152 @@ class DeepspeedExecutor:
             raise MetaflowException(
                 "Do not specify the --master_addr in your current.run.deepspeed args. Metaflow will set this for you."
             )
-
         my_ip = socket.gethostbyname(socket.gethostname())
         deepspeed_args.extend(["--master_addr", my_ip])
-
         cmd.extend(deepspeed_args)
 
+        # Construct rest of command starting with the entrypoint.
         if entrypoint is not None:
             cmd.append(entrypoint)
         else:
             raise MetaflowException(
                 "current.deepspeed.run(..., entrypoint=<SCRIPT>, ...) arg must be specified."
             )
+        # cmd.extend(entrypoint_args)
+        if entrypoint_args is not None and isinstance(entrypoint_args, dict):
+            for arg, val in entrypoint_args.items():
+                if val == "" or val == None:
+                    cmd.append("--%s" % arg)
+                else:
+                    cmd.extend(["--%s" % arg, str(val)])
+        elif entrypoint_args is not None and isinstance(entrypoint_args, list):
+            cmd.extend(entrypoint_args)
 
-        cmd.extend(entrypoint_args)
-
-        print(" ".join(cmd))
-        
-        # write env_var=value to file
-        env = os.environ.copy()
+        # Write env_var=value to file.
+        # Deepspeed automatically looks for this file to prepend the variables to its launcher command.
         with open(DEEPSPEED_ENV_FILE, "w") as f:
-            for k, v in env.items():
-                f.write(f"{k}={v}\n")
+            for k, v in os.environ.items():
+                if k == "METAFLOW_INIT_SCRIPT":
+                    continue  # Don't pass these to deepspeed. Because of how deepspeed reads env vars and sets them in runner commands.
+                    # TODO: investigate PR to deepspeed to change how it uses .deepspeed_env to set env vars on workers.
+                else:
+                    json_string = json.dumps(v)
+                    f.write(f"{k}={json_string}\n")
 
-        # run the command
+        # Launch the Deepspeed run.
         with subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            cmd,
+            stdout=subprocess.PIPE,  # pipe to Metaflow stdout. TODO: How to handle progress bar buffering like TQDM.
+            stderr=subprocess.STDOUT,
         ) as process:
             while process.poll() is None:
                 stdout = process.stdout.read1()
                 try:
                     text = stdout.decode("utf-8")
                 except UnicodeDecodeError:
-                    # TODO: This print feels bad, maybe remove - even better,
-                    # figure out how to handle the edge decoding cases gracefully.
-                    # print("UnicodeDecodeError, skipping decoding of problematic bytes: %s" % stdout)
                     text = ""
-
-                print(text, end="", flush=True)
-                # TODO (Eddie): what is strat for dynamic cards? stuff `text` somewhere?
+                print(
+                    text, end="", flush=True
+                )  # TODO: explore other mechanisms to flush buffer correctly, especially in progress bar case.
 
             if process.returncode != 0:
                 raise DeepspeedException(cmd)
 
     def run(
         self,
-        deepspeed_args=[],
-        entrypoint=None,
-        entrypoint_args=[],
-        deepspeed_config=None,
-        push_results_dir_to_s3=False,
-        local_output_dir=None,
-        s3_output_dir=None,
-    ):
+        deepspeed_args: Union[List[str], Dict[str, str]] = [],
+        entrypoint: str = None,
+        entrypoint_args: Union[List[str], Dict[str, str]] = [],
+        push_results_dir_to_s3: bool = False,
+        local_output_dir: str = None,
+        s3_output_dir: str = None,
+        s3_root: str = None,
+    ) -> str:
         from metaflow import current, S3
-        node_index = current.parallel.node_index
+
+        node_index = current.parallel.node_index  # assumes parallel
         if push_results_dir_to_s3:
+            # TODO: Annoying place for this check. Consider moving the S3 push args into the decorator itself, so can be checked at flow init instead.
             if local_output_dir is None:
-                raise MetaflowException("current.deepspeed.run must specify local_output_dir if push_results_dir_to_s3 is True")
+                raise MetaflowException(
+                    "current.deepspeed.run must specify local_output_dir if push_results_dir_to_s3 is True"
+                )
             elif s3_output_dir is None:
-                s3_output_dir = os.path.relpath(local_output_dir, os.getcwd())
-        with S3(run=self.flow) as s3:
-            if node_index == 0: # control node
-                self._exec_cmd(deepspeed_args, entrypoint, entrypoint_args, deepspeed_config)
-                s3.put(CONTROL_TASK_DONE_PATH, json.dumps({DEEPSPEED_JOB_COMPLETE_VAR: True}))
-            else: # worker node
-                control_done = False
-                while not control_done:
-                    time.sleep(self.worker_polling_freq)
-                    try:
-                        control_done = json.loads(s3.get(CONTROL_TASK_DONE_PATH).blob)[DEEPSPEED_JOB_COMPLETE_VAR]
-                    except metaflow.plugins.datatools.s3.s3.MetaflowS3NotFound:
-                        control_done = False
-                        continue
-            if push_results_dir_to_s3:
-                if not os.path.exists(local_output_dir):
-                    print(f"Deepspeed process completed, and local_output_dir `{local_output_dir}` does not exist, skipping push to S3.")
-                    return
-                if not os.path.isdir(local_output_dir):
-                    print(f"Deepspeed process completed, and local_output_dir `{local_output_dir}` is not a directory, skipping push to S3.")
-                    return
-                if len(os.listdir(local_output_dir)) == 0:
-                    print(f"Deepspeed process completed, and local_output_dir `{local_output_dir}` is empty, skipping push to S3.")
-                    return
-                filepath_tuples = []
-                for path, subdirs, files in os.walk(local_output_dir):
-                    for fname in files:
-                        filepath_tuples.append(
-                            (
-                                f"{s3_output_dir}/{str(node_index)}/{os.path.relpath(os.path.join(path, fname), local_output_dir)}",
-                                os.path.join(path, fname)
-                            )
+                if local_output_dir.startswith("/"):
+                    s3_output_dir = local_output_dir[1:]
+                else:
+                    s3_output_dir = local_output_dir  # os.path.relpath(local_output_dir, os.getcwd())
+
+        if s3_root is not None:
+            s3 = S3(s3root=s3_root)
+        else:
+            s3 = S3(run=self.flow)
+
+        # Run the distributed job
+        if node_index == 0:  # control node
+            self._exec_cmd(deepspeed_args, entrypoint, entrypoint_args)
+            s3.put(
+                CONTROL_TASK_DONE_PATH, json.dumps({DEEPSPEED_JOB_COMPLETE_VAR: True})
+            )
+        else:  # worker node
+            control_done = False
+            while not control_done:
+                time.sleep(self.worker_polling_freq)
+                try:
+                    control_done = json.loads(s3.get(CONTROL_TASK_DONE_PATH).blob)[
+                        DEEPSPEED_JOB_COMPLETE_VAR
+                    ]
+                except metaflow.plugins.datatools.s3.s3.MetaflowS3NotFound:
+                    control_done = False
+                    continue
+
+        # Push results to S3
+        if push_results_dir_to_s3:
+            if not os.path.exists(local_output_dir):
+                print(
+                    f"Deepspeed process completed, and local_output_dir `{local_output_dir}` does not exist, skipping push to S3."
+                )
+                return
+            if not os.path.isdir(local_output_dir):
+                print(
+                    f"Deepspeed process completed, and local_output_dir `{local_output_dir}` is not a directory, skipping push to S3."
+                )
+                return
+            if len(os.listdir(local_output_dir)) == 0:
+                print(
+                    f"Deepspeed process completed, and local_output_dir `{local_output_dir}` is empty, skipping push to S3."
+                )
+                return
+            filepath_tuples = []
+            for path, subdirs, files in os.walk(local_output_dir):
+                for fname in files:
+                    filepath_tuples.append(
+                        (
+                            f"{s3_output_dir}/{str(node_index)}/{os.path.relpath(os.path.join(path, fname), local_output_dir)}",
+                            os.path.join(path, fname),
                         )
-                print(f"Pushing outputs in {local_output_dir} from node {node_index} to S3...")
-                path_result = s3.put_files(filepath_tuples)
-                s3_output_dir_full = f"{path_result[0][1].split(f'/{s3_output_dir}')[0]}/{s3_output_dir}"
-                print(f"Push completed. Results available at {s3_output_dir_full}.")
-                print(f"\n\nTo access the S3 results, instantiate Metaflow's S3 client with: with S3(run=Run('{current.flow_name}/{current.run_id}')) as s3: ...")
-                print(f"\nTo view metadata from this node use: s3.list_paths(['{s3_output_dir}/{node_index}'])")
-                print(f"\nTo recurisvely download everything in {s3_output_dir_full} use: s3.get_recursive(keys=['{s3_output_dir}'])\n\n")
-            
+                    )
+            print(
+                f"Pushing outputs in {local_output_dir} from node {node_index} to S3..."
+            )
+            path_result = s3.put_files(filepath_tuples)
+            s3_output_dir_full = (
+                f"{path_result[0][1].split(f'/{s3_output_dir}')[0]}/{s3_output_dir}"
+            )
+            print(f"Push completed. Results available at {s3_output_dir_full}.")
+            print(
+                f"\nTo access the S3 results, instantiate Metaflow's S3 client using:\n\twith S3(run=Run('{current.flow_name}/{current.run_id}')) as s3: ..."
+            )
+            print(
+                f"\nTo view metadata from this node use:\n\ts3.list_paths(['{s3_output_dir}/{node_index}'])"
+            )
+            print(
+                f"\nTo recurisvely download everything in {s3_output_dir_full} use:\n\ts3.get_recursive(keys=['{s3_output_dir}'])\n\n"
+            )
+            s3.close()
+            return s3_output_dir_full
+
+        s3.close()
 
     def _scan_cmd(self, host):
         return ["ssh-keyscan", "-H", host]
@@ -186,17 +255,28 @@ class DeepspeedExecutor:
 
 
 class DeepspeedDecorator(ParallelDecorator):
+
     name = "deepspeed"
     defaults = {"all_nodes_started_timeout": 90, "worker_polling_freq": 5}
     IS_PARALLEL = True
 
-    requires_passwordless_ssh = True
+    requires_passwordless_ssh = (
+        True  # modifies how @kubernetes runs command in docker container
+    )
 
     def _setup_current(self, hosts, n_slots_per_host, is_gpu, flow):
         from metaflow import current
 
         current._update_env(
-            {"deepspeed": DeepspeedExecutor(hosts, n_slots_per_host, is_gpu, flow, self.attributes['worker_polling_freq'])}
+            {
+                "deepspeed": DeepspeedExecutor(
+                    hosts,
+                    n_slots_per_host,
+                    is_gpu,
+                    flow,
+                    self.attributes["worker_polling_freq"],
+                )
+            }
         )
 
     def step_init(self, flow, graph, step, decos, environment, flow_datastore, logger):
@@ -273,6 +353,7 @@ class DeepspeedDecorator(ParallelDecorator):
             hosts=hosts, n_slots_per_host=self.n_slots, is_gpu=self.is_gpu, flow=flow
         )
         return hosts
+
 
 def setup_mpi_env(
     run, ubf_context, all_nodes_started_timeout, interval, n_slots, is_k8s
@@ -397,5 +478,7 @@ class DeepspeedException(MetaflowException):
     headline = ""
 
     def __init__(self, cmd):
-        msg = "The Deepspeed command \n\n{}\n\nfailed to complete.".format(" ".join(cmd))
+        msg = "The Deepspeed command \n\n{}\n\nfailed to complete.".format(
+            " ".join(cmd)
+        )
         super(DeepspeedException, self).__init__(msg)
