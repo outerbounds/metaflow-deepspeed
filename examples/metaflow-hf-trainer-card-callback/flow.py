1d2555b3e2b8f8078ecc2214a7ff675cf3506ac3
Valay Dave <valaygaurang@gmail.com>
2024-04-30 17:08:26 -0400

    [hf-callback] General mechanism to track HF Jobs (#8)
    * [hf-callback] General mechanisms to track HF Jobs
    
    ## Example
    ```
    # import the decorator compatible with callback
    from metaflow.plugins.hf_info_card import huggingface_card
    
    class HFCBFlow(FlowSpec):
    
        @huggingface_card
        @step
        def start(self):
            my_hf_trainer_fn()
            self.next(self.end)
    
        @step
        def end(self):
            pass
    
    ```
    
    ```
    # in your training function drop the callback
    from metaflow.plugins.hf_info_card import MetaflowHuggingFaceCardCallback
    
    def my_hf_trainer_fn():
        ...
        ...
        Trainer(
            ...
            callbacks=[MetaflowHuggingFaceCardCallback(
                tracked_metrics=["val_loss", "train_loss"],
            )]
    
        )
    ```

---
+++ examples/metaflow-hf-trainer-card-callback/flow.py
@@ -0,0 +1,51 @@
+from metaflow import FlowSpec, step, deepspeed, kubernetes, current
+from metaflow.profilers import gpu_profile
+from metaflow import huggingface_card
+
+N_NODES = 2
+IMAGE = "docker.io/eddieob/deepspeed:6"
+MEMORY = "32000"
+N_GPU = 1
+N_CPU = 2
+
+
+class MetaflowDeepspeedHFCallbackExample(FlowSpec):
+
+    checkpoint_dir = "training_outputs"
+
+    @step
+    def start(self):
+        self.next(self.train, num_parallel=N_NODES)
+
+    @huggingface_card
+    @gpu_profile(interval=1)
+    @kubernetes(image=IMAGE, memory=MEMORY, cpu=N_CPU, gpu=N_GPU)
+    @deepspeed
+    @step
+    def train(self):
+
+        # Run the deepspeed task.
+        # In order to use the Azure Blob client, we pass run-id and flow-name to the entrypoint.
+        # This is necessary because deepspeed launches the entrypoint as a subprocess, 
+        # so the script does not have access to the Metaflow current object to pull these values.
+        current.deepspeed.run(
+            entrypoint="train.py",
+            entrypoint_args={
+                "run-id": current.run_id,
+                "flow-name": current.flow_name,
+                "checkpoint-dir": self.checkpoint_dir,
+            },
+        )
+        self.next(self.join)
+
+    @step
+    def join(self, inputs):
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+
+if __name__ == "__main__":
+    MetaflowDeepspeedHFCallbackExample()
